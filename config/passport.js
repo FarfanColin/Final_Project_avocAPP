b4f53fdd382f462f9aaf6e46363a941e5dd866e0
FarfanColin <cjfc12345@hotmail.com>
2020-12-10 12:52:21 +0000

    Nine video done

--- config/passport.js
+++ config/passport.js
@@ -2,37 +2,78 @@ var passport = require('passport');
 var User = require('../models/user');
 var LocalStrategy = require('passport-local').Strategy;
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user){
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
         done(err, user);
     });
 });
 
 passport.use('local.signup', new LocalStrategy({
-    usernameField:'email',
+    usernameField: 'email',
     passwordField: 'password',
     passReqToCallback: true
-}, function(req, email, password, done){
-    User.findOne({'email': email}, function(err, user){
-        if(err){
+}, function (req, email, password, done) {
+    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid password').notEmpty().isLength({ min: 4 });
+    var errors = req.validationErrors();
+    if (errors) {
+        var messages = [];
+        errors.forEach(function (error) {
+            messages.push(error.msg);
+        });
+        return done(null, false, req.flash('error', messages));
+    }
+    User.findOne({ 'email': email }, function (err, user) {
+        if (err) {
             return done(err);
         }
         if (user) {
             var messages = "Email has been taken"
-  return done(null, false, req.flash('error',messages));
+            return done(null, false, req.flash('error', messages));
         }
         var newUser = new User();
         newUser.email = email;
         newUser.password = newUser.encryptPassword(password);
-        newUser.save(function(err, result) {
-            if(err) {
+        newUser.save(function (err, result) {
+            if (err) {
                 return done(err);
             }
             return done(null, newUser);
         });
     });
 }))
+
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, function (req, email, password, done) {
+    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid password').notEmpty();
+    var errors = req.validationErrors();
+    if (errors) {
+        var messages = [];
+        errors.forEach(function (error) {
+            messages.push(error.msg);
+        });
+        return done(null, false, req.flash('error', messages));
+    }
+    User.findOne({ 'email': email }, function (err, user) {
+        if (err) {
+            return done(err);
+        }
+        if (!user) {
+            var messages = "No user found"
+            return done(null, false, req.flash('error', messages));
+        }
+        if (!user.validPassword(password)) {
+            var messages = "Wrong password"
+            return done(null, false, req.flash('error', messages));
+        }
+        return done(null, user);
+    });
+}));
