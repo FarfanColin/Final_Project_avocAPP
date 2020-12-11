28736c45a212f5831d53df7de2fd2c6daf2056e8
FarfanColin <cjfc12345@hotmail.com>
2020-12-11 22:56:33 +0000

    END

--- routes/user.js
+++ routes/user.js
@@ -3,56 +3,84 @@ var router = express.Router();
 var csrf = require('csurf');
 var passport = require('passport');
 
+var Order = require('../models/order');
+var Cart = require('../models/cart');
+
 var csrfProtection = csrf();
 router.use(csrfProtection);
 
-router.get('/profile', isLoggedIn, function(req, res, next) {
-    res.render('user/profile');
-  });
-  
-  router.get('/logout', function (req, res, next) {
-    req.logout();
-    res.redirect('/');
-});
-
-router.use('/', notLoggedIn, function(req, res, next) {
-    next();
-});
-
-router.get('/signup', function(req, res, next) {
-    var messages = req.flash('error');
-    res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
-  })
-  
-  router.post('/signup', passport.authenticate('local.signup', {
-    successRedirect: '/user/profile',
-    failureRedirect: '/user/signup',
-    failuereFlash: true
-  }));
-  
-  router.get('/signin', function(req, res, next) {
-    var messages = req.flash('error');
-    res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
-  });
-  
-  router.post('/signin', passport.authenticate('local.signin', {
-    successRedirect: '/user/profile',
-    failureRedirect: '/user/signin',
-    failuereFlash: true
-  }));
-  
-  module.exports = router;
-
-  function isLoggedIn(req, res, next) {
-      if(req.isAuthenticated()) {
-          return next();
-      }
-      res.redirect('/');
-  }
-
-  function notLoggedIn(req, res, next) {
-    if(!req.isAuthenticated()) {
-        return next();
+router.get('/profile', isLoggedIn, function (req, res, next) {
+  Order.find({user: req.user}, function(err, orders) {
+    if(err) {
+      return res.write('Error');
     }
-    res.redirect('/');
+    var cart;
+    orders.forEach(function(order) {
+      cart = new Cart(order.cart);
+      order.items = cart.generateArray();
+    });
+    res.render('user/profile', {orders:orders });
+  });
+});
+
+router.get('/logout', function (req, res, next) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.use('/', notLoggedIn, function (req, res, next) {
+  next();
+});
+
+router.get('/signup', function (req, res, next) {
+  var messages = req.flash('error');
+  res.render('user/signup', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
+});
+
+router.post('/signup', passport.authenticate('local.signup', {
+  failureRedirect: '/user/signup',
+  failuereFlash: true
+}), function (req, res, next) {
+  if (req.session.oldUrl) {
+    var oldUrl = req.session.oldUrl;
+    req.session.oldUrl = null;
+    res.redirect(oldUrl);
+  } else {
+    res.redirect('/user/profile');
+  }
+});
+
+router.get('/signin', function (req, res, next) {
+  var messages = req.flash('error');
+  res.render('user/signin', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
+});
+
+router.post('/signin', passport.authenticate('local.signin', {
+  failureRedirect: '/user/signin',
+  failuereFlash: true
+}), function (req, res, next) {
+  if (req.session.oldUrl) {
+    var oldUrl = req.session.oldUrl;
+    req.session.oldUrl = null;
+    res.redirect(oldUrl);
+  } else {
+    res.redirect('/user/profile');
+  }
+});
+
+
+module.exports = router;
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+}
+
+function notLoggedIn(req, res, next) {
+  if (!req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
 }
