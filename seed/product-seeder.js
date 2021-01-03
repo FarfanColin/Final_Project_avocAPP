23b7c664d961e2f86c916d994936ee27b1eef35a
FarfanColin <cjfc12345@hotmail.com>
2021-01-03 21:04:30 +0000

    Images

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -6,36 +6,67 @@ mongoose.connect('mongodb://localhost:27017/shopping');
 
 var products = [
     new Product({
-        imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
-        title: 'Gothic Game',
-        description: 'Awesome Game',
+        imagePath: 'images/SOY.png',
+        title: 'SOY',
+        description: 'Chunks of protein. None of the meat Meat-free protein just how we like it. Naturally.This product is suitable for Vegetarians and vegans. Meat alternative',
+        price: 5
+    }),
+    new Product({
+        imagePath: 'images/MILK.png',
+        title: 'MILK',
+        description: 'Great frothy alternative to milk produced by cows,made for baristas and those who like to practice their skills at home!',
+        price: 2
+    }),
+    new Product({
+        imagePath: 'images/CHEESE.png',
+        title: 'CHEESE',
+        description: 'These vegan cheese is suitable for vegans and vegetarians. Plus they are low in saturated fat, low in sugar and a great source of protein and fibre... they have everything you need!',
+        price: 5
+    }),
+    new Product({
+        imagePath: 'images/PROTEIN.png',
+        title: 'PROTEIN',
+        description: 'This supplement is made by vegan nutritionists for vegans. Its 100% natural, plant based recipe uses only the finest pure ingredients. Zero cruelty option for vegans.',
+        price: 25
+    }),
+    new Product({
+        imagePath: 'images/TOFU.png',
+        title: 'TOFU',
+        description: 'Made of condensed soy milk that is pressed into solid white blocks in a process quite similar to cheesemaking. Suitable for vegans and vegetarians',
+        price: 5
+    }),
+    new Product({
+        imagePath: 'images/B12.png',
+        title: 'B12',
+        description: 'Vegan Vitamin B12 is a must-have supplement for anyone following a vegan or plant-based lifestyle. Our super-convenient tablets make it easy for you to hit you recommended requirements of the essential vitamins B12.',
         price: 15
     }),
     new Product({
-        imagePath: 'https://d1r7xvmnymv7kg.cloudfront.net/sites_products/darksouls3/assets/img/DARKSOUL_facebook_mini.jpg',
-        title: 'Otro uno',
-        description: 'God',
-        price: 14
+        imagePath: 'images/DOUGHNUTS.png',
+        title: 'DOUGHNUTS',
+        description: 'Vegan donuts, including caramel, vegan chocolate and vegan lemon is a perfect option to share anytime and anywhere.',
+        price: 20
     }),
     new Product({
-        imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/51/Minecraft_cover.png',
-        title: 'Otro dos',
-        description: 'Great',
-        price: 13
+        imagePath: 'images/CHOCOLATE.png',
+        title: 'CHOCOLATE',
+        description: 'For all of you chocolate-loving vegans out there! Vegan chocolate. Chocolate is made from cacao beans, which are grown on cacao trees.',
+        price: 15
     }),
     new Product({
-        imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5b/Astroneer_Image1.png',
-        title: 'Otro tres',
-        description: 'Great',
-        price: 12
+        imagePath: 'images/SPAGHETTI.png',
+        title: 'SPAGHETTI',
+        description: 'Gluten free, Sugar free, Fat free, Low carbohydrate. Tastes great! Source of fibre and dairy free.',
+        price: 6
     }),
     new Product({
-        imagePath: 'https://upload.wikimedia.org/wikipedia/commons/8/80/Roblox_Logo_Black.svg',
-        title: 'Otro cuatro',
-        description: 'Great',
-        price: 11
+        imagePath: 'images/SEITAN.png',
+        title: 'SEITAN',
+        description: 'A health care product, thanks to its excellent properties can be used to obtain a healthy diet, with ingredients that can prevent degenerative diseases. It provides high amounts of protein and fiber, is low in carbohydrates.',
+        price: 10
     })
 ];
+
 
 var done = 0;
 for (var i = 0; i < products.length; i++) {
