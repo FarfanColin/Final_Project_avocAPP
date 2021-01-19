680aa2b61f1574dbadd1b815e294364c730120fc
FarfanColin <61020345+FarfanColin@users.noreply.github.com>
2021-01-19 14:22:00 +0000

    1901_14

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var adminRoutes = require('./routes/admin');
 var app = express();
 
 //If the database is not created, automatically it will be created
-mongoose.connect('mongodb://localhost:27017/myDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://Christian:SmlLKKyv3UVB4KZ4@cluster0.dgy6w.mongodb.net/myDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 require('./config/passport');
 
 // Setting the hbs engine
